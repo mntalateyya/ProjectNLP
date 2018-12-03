4bef0ba674dc441b87ce3465bf92e95a0232c202
Mohammed Alateyya <mnur@cmu.edu>
2018-12-03 13:18:37 +0300

    recursive search & subtree

--- src/test.py
+++ src/test.py
@@ -1,17 +1,37 @@
 #!/usr/bin/python3
 
-from depgraph import DepGraph
-from utils import Document
+from sgraph import SentenceGraph
 from pycorenlp import StanfordCoreNLP
+from patterns import patterns
+import re, pprint
 
 client = StanfordCoreNLP('http://localhost:9000')
 
-with open('../data/set1/a2.txt') as f:
-    doc = Document(f.read())
-print(len(doc.sentences))
-for sent in doc.getSentences():
-    dg = DepGraph(client, sent)
-    res = dg.match('VBD', [('nsubj', None), ('dobj', None)])
+with open('data/set1/a2.txt') as f:
+    corenlp_out = eval(client.annotate(f.read(), properties={
+                'annotators': 'depparse, pos, lemma',
+                'corenlp_outputFormat': 'json'
+            }))
+'''
+# from https://stackoverflow.com/questions/6116978/how-to-replace-multiple-substrings-of-a-string
+rep = {"true": "True", "false": "False"} # define desired replacements here
+# use these three lines to do the replacement
+rep = { re.escape(k): v for k, v in rep.items() }
+pattern = re.compile("|".join(rep.keys()))
+corenlp_out = eval(pattern.sub(lambda m: rep[re.escape(m.group(0))], corenlp_out))
+
+pp = pprint.PrettyPrinter(indent=4)
+pp.pprint(corenlp_out['corefs'])'''
+
+for sent in corenlp_out['sentences']:
+    dg = SentenceGraph(sent)
+    res = dg.match(patterns[0])
     if res:
-        verb, subj, obj = res
-        print('Who {} {}? {}'.format(dg.tokens[verb], dg.tokens[obj], dg.tokens[subj]))
+        print('Q: Who is ({})? A: ({})'.format(
+            dg.subtree(res['subject']), 
+            ' '.join(map(lambda i: dg.tokens[i]['word'], range(res['det'], res['pred']+1)))))
+    res = dg.match(patterns[1])
+    if res:
+        print('Q: Who is ({})? A: ({})'.format(
+            dg.subtree(res['pred']).replace(dg.subtree(res['subject']), ''),
+            dg.subtree(res['subject'])))
