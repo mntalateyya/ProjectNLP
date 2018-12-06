777ddf2349036674975254adebce7ed3970aa95b
Mohammed Alateyya <mnur@cmu.edu>
2018-12-06 12:19:33 +0300

    separate Q gen. from testing

--- src/patterns.py
+++ src/patterns.py
@@ -1,50 +1,73 @@
+from pattern.en import conjugate
 '''
-patterns to feed into SentenceGraph.match. A pattern matches nodes in the dependency graph
+List of pattern-template tuples. A pattern matches nodes in the dependency graph
 and is a dictionary that has zero or more of the following 4 keys:
 - name: if successfully matched the returned dictionary will have this as a key mapping
         to the index of this node.
 - attributes: a dictionary of attributes this node has to have. these attributes are the
         token attrubutes in CoreNLP output. e.g. `'attributes': { 'pos': 'NNP' }`. The attribute
-        value can also be a set (or list, prefer set), in which case it matches if one of
-        the values match. `'attributes': { 'pos': { 'VBD', 'VBN' }}`
+        value can also be '!value' in which case, it matches anything but value and can also be
+        a set (or list, prefer set), in which case it matches if one of
+        the values match. `'attributes': { 'pos': { 'VBD', 'VBN' }}` (!value not permitted here).
 - 'has_deps': depndency relations this node should have, where this node is the governor
         of the relation. This a dictionary where keys are relations and values are 
         recursive patterns.
 - 'has_not': a list of relations this node should not have.
 '''
 patterns = [
-    {
+    ({
         'name': 'pred',
         'has_deps': {
             'det': {
                 'name': 'det',
-                'attributes': { 'word': 'a' }},
-            'cop': { 'attributes': { 'word': 'is' }},
+                'attributes': {'word': 'a'}},
+            'cop': {'attributes': {'word': 'is'}},
             'nsubj': {
                 'name': 'subject',
-                'attributes': { 'pos': 'NNP' }
+                'attributes': {'pos': 'NNP'}
             },
         }
     },
-    {
+    lambda sg, res: (
+        'Who is {}?'.format(sg.subtree(res['subject'])),
+        'A: {}'.format(' '.join(
+            map(lambda i: sg.tokens[i]['word'], range(res['det'], res['pred']+1))))
+    )),
+
+    ({
         'name': 'pred',
         'has_deps': {
             'det': {
                 'name': 'det',
-                'attributes': { 'word': 'the' }},
-            'cop': { 'attributes': { 'word': 'is' }},
+                'attributes': {'word': 'the'}},
+            'cop': {'attributes': {'word': 'is'}},
             'nsubj': {
                 'name': 'subject'
             },
         }
     },
-    {
+    lambda sg, res: (
+        'Who {}?'.format(sg.subtree(res['pred']).replace(
+            sg.subtree(res['subject']), '')),
+        'A: {}'.format(sg.subtree(res['subject']))
+    )),
+
+    ({
         'name': 'verb',
-        'attributes': { 'pos': 'VBD' },
+        'attributes': {'pos': 'VBD'},
         'has_deps': {
-            'nmod:in': { 'name': 'time' },
-            'nsubj': { 'name': 'subject', 'attributes': { 'pos': '!PRP' }}
-        }, 
+            'nmod:in': {'name': 'time'},
+            'nsubj': {'name': 'subject', 'attributes': {'pos': '!PRP'}}
+        },
         'has_not': ['cc']
-    }
+    },
+    lambda sg, res: ( # FIXME, ensure in <time> occurs before subject
+        'When did {} {} {}?'.format(
+            sg.subtree(res['subject']),
+            conjugate(sg.tokens[res['verb']]['word'], 'inf'),
+            ' '.join(map(lambda i: sg.tokens[i]['word'], range(
+                res['verb']+1, sg.length-1)))
+        ),
+        'A: {}'.format(sg.subtree(res['time']))
+    )),
 ]
