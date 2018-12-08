35c9e3c88199e1c257b6cbecc184ef5ddfd39821
Mohammed Alateyya <mnur@cmu.edu>
2018-12-08 12:30:21 +0300

    read file names from argv

--- src/test.py
+++ src/test.py
@@ -3,15 +3,29 @@
 from sgraph import SentenceGraph
 from pycorenlp import StanfordCoreNLP
 from patterns import patterns
+from sys import argv
 # import re, pprint
 
 client = StanfordCoreNLP('http://localhost:9000')
 
-with open('data/set1/a2.txt') as f:
-    corenlp_out = eval(client.annotate(f.read(), properties={
-                'annotators': 'depparse, pos, lemma',
-                'corenlp_outputFormat': 'json'
-            }))
+for fname in argv[1:]:
+    with open(fname) as f:
+        corenlp_out = eval(client.annotate(f.read(), properties={
+                    'annotators': 'depparse, pos, lemma, ner    ',
+                    'corenlp_outputFormat': 'json'
+                }))
+
+    for sent in corenlp_out['sentences']:
+        sg = SentenceGraph(sent)
+        for i, (pat, tmpl) in enumerate(patterns):
+            res = sg.match(pat)
+            if res is not None:
+                q = tmpl(sg, res)
+                if q[0]:
+                    print('Original:', ' '.join(map(lambda i: sg.tokens[i]['word'], range(1, sg.length))))
+                    print('Q[{}]:'.format(i), q[0], '\n', 'A:', q[1])
+                    print()
+
 '''
 # from https://stackoverflow.com/questions/6116978/how-to-replace-multiple-substrings-of-a-string
 # required if coref used because it returns true and false in lowercase
@@ -24,14 +38,3 @@ corenlp_out = eval(pattern.sub(lambda m: rep[re.escape(m.group(0))], corenlp_out
 pp = pprint.PrettyPrinter(indent=4)
 pp.pprint(corenlp_out['corefs'])
 '''
-
-for sent in corenlp_out['sentences']:
-    sg = SentenceGraph(sent)
-    for i, (pat, tmpl) in enumerate(patterns):
-        res = sg.match(pat)
-        if res is not None:
-            q = tmpl(sg, res)
-            if q[0]:
-                print('Original:', ' '.join(map(lambda i: sg.tokens[i]['word'], range(1, sg.length))))
-                print('Q[{}]:'.format(i), q[0], '\n', 'A:', q[1])
-                print()
