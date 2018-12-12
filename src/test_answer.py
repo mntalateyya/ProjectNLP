bb4fcef0f67ec0e096add101fd229577187a2bb0
Mohammed Alateyya <mnur@cmu.edu>
2018-12-12 16:18:53 +0300

    test_answer

---
+++ src/test_answer.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+from sys import argv
+from pycorenlp import StanfordCoreNLP
+from answer_heuris import answer
+from sgraph import SentenceGraph
+
+#with open(argv[1]) as f:
+#    text = f.readline()
+text = 'John is the best football player. John likes apples. John met Jane yesterday. John ate with Jane. John was killed.'
+
+questions = [
+    #'Who is John?',
+    #'Who is the best football player?',
+    #'Who likes apples?',
+    #'Who did John meet?',
+    'Who did John eat with?',
+    #'Who was killed?'
+]
+
+client = StanfordCoreNLP('http://localhost:9000')
+
+sentences =  corenlp_out = eval(client.annotate(text, properties={
+    'annotators': 'depparse, lemma',    
+}))['sentences']
+sentences = list(map(SentenceGraph, sentences))
+
+parse_questions = [
+    SentenceGraph(
+        eval(client.annotate(q, properties={ 'annotators': 'depparse, lemma' }))['sentences'][0]
+    ) for q in questions
+]
+
+for i, q in enumerate(parse_questions):
+    ans = answer(sentences, q)
+    print(questions[i])
+    print(ans if ans else 'NOT FOUND')
+    print()
